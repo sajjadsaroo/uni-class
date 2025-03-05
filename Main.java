7d6213ed94fd2f0a2fe483a90f1c04b4b564f7ab
Macsaroo <sajjad.sarookhani@gmail.com>
2025-03-05 11:07:58 +0330

    add main class and set person 1-5 and student 1-3 and print students

--- Main.java
+++ Main.java
@@ -1,2 +1,31 @@
+import base.*;
+import uni.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Ali Ahmadi", "1234567890");
+        Person p2 = new Person("Sara Karimi", "0987654321");
+        Person p3 = new Person("Reza Mohammadi", "1122334455");
+        Person p4 = new Person("Neda Hosseini", "2233445566");
+        Person p5 = new Person("Hassan Ramezani", "3344556677");
+
+        Major cs = new Major("Computer Science", 50);
+        Major math = new Major("Mathematics", 40);
+
+        Student s1 = new Student(p1.id, 401, cs.id);
+        Student s2 = new Student(p2.id, 401, cs.id);
+        Student s3 = new Student(p5.id, 403, math.id);
+
+        System.out.println("Students List:");
+        for (Student s : Student.studentList) {
+            Person person = Person.findByID(s.personId);
+            if (person != null) {
+                System.out.println("Student ID: " + s.studentID + ", Name: " + person.name);
+            } else {
+                System.out.println("Student ID: " + s.studentID + " -> Person not found!");
+            }
+        }
+
+    }
 }
